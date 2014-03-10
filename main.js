4b3933a19921b1b4263194e0991d1e3e719a3276
David Fisher <tibbon@gmail.com>
2014-03-10 13:45:16 -0400

    Catchin gup

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  var data = [4, 8, 15, 16, 23, 42];
+  var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 800]);
+
+  d3.select(".chart") // Selects the chart (container)
+    .selectAll("div") // This is what we *will* have, also what's being appended.
+      .data(data)     // This will be the data that maps to our divs
+    .enter().append("div") // Enter is a join. This is what will happen with new data. We'll append it.
+      .style("width", function(d) { return x(d) + "px"; }) // Just setting the style
+      .text(function(d) { return d; }); // Setting the text. Note the D is the same, passed a function
+
+};
